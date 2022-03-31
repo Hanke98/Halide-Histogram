7be389c24e5def1cfacdb57d8945490ddab03290
booleangabs <gabrieltavares1303@gmail.com>
2022-03-31 20:13:07 -0300

    Histogram computation

---
+++ 03-Histogram-AOT/histogram.cpp
@@ -0,0 +1,29 @@
+/*
+g++ -fno-rtti -std=c++17 histogram.cpp -o histogram -I$HALIDE_ROOT/include \
+    -I$HALIDE_ROOT/tools -L$HALIDE_ROOT/lib -lHalide -lz -ldl \
+    -lpthread
+*/
+
+#include "Halide.h"
+#include <stdio.h>
+
+using namespace Halide;
+using namespace Halide::ConciseCasts;
+
+int main(int argc, char **argv) {
+    Func histogram("histogram");
+    Var x("x"), y("y"), i("i"), ii("ii"), io("io");
+
+    Param<int> channel;
+    ImageParam input(type_of<uint8_t>(), 3);
+
+    histogram(i) = 0;
+    
+    RDom r(input);
+    histogram(input(r.x, r.y, channel)) += 1;
+    histogram.compute_root();
+    histogram.compile_to_static_library("histogram", {input, channel}, "histogram");
+    std::cout << "Halide pipeline compiled, but not yet run." << std::endl;
+
+    return 0;
+}
---
+++ 03-Histogram-AOT/main.cpp
@@ -0,0 +1,39 @@
+/*
+g++ -fno-rtti -std=c++17 main.cpp histogram.a -o main -I$HALIDE_ROOT/include \
+    -I$HALIDE_ROOT/tools -L$HALIDE_ROOT/lib -lHalide -lz -ldl \
+    -lpthread -ljpeg `libpng-config --cflags --ldflags`
+*/
+
+#include <iostream>
+#include <fstream>
+#include "HalideBuffer.h"
+#include "halide_image_io.h"
+#include "histogram.h"
+
+using namespace Halide::Runtime;
+using namespace Halide::Tools;
+
+int main(int argc, char **argv) {
+    if(argc < 3) {
+        puts("Usage: ./main path_input channel");
+        return 1;
+    }
+    const char * path_input = argv[1];
+    const int channel = (int) atof(argv[2]);;
+    
+    Buffer<uint8_t> input = load_image(path_input);
+    Buffer<int32_t> output({256});
+
+    histogram(input, channel, output);
+    int32_t * data = output.data();
+    std::ofstream outFile("results/plotme.dat");
+    for (int i = 0; i < 255; i++) {
+        outFile << i << " " << * (data + i) << "\n";
+        std::cout << * (data + i) << " ";
+    }
+    outFile.close();
+    std::cout << std::endl;
+
+    std::cout << "Success!" << std::endl;
+    return 0;
+}
